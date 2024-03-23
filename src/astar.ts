4ad8bc855d87e31e7128d095d3d0144065dc5630
Josh Cox <josh.cox@hingehealth.com>
2024-03-23 11:32:46 -0500

    Separate out nodeSet and openSet behavior to simplify search algo

--- src/astar.ts
+++ src/astar.ts
@@ -10,42 +10,71 @@ function assertsIsDefined<T>(value: T | undefined | null): asserts value is T {
   }
 }
 
+class NodeSet<Node extends INode<IData>> {
+  protected ids = new Set<string>();
+
+  public get length() {
+    return this.ids.size;
+  }
+
+  public has(node: Node) {
+    return this.ids.has(node.id());
+  }
+
+  public push(node: Node): NodeSet<Node> {
+    this.ids.add(node.id());
+    return this;
+  }
+}
+
+class OpenSet<Node extends INode<IData>> extends NodeSet<Node> {
+  private queue = new TinyQueue<Node>([], (a, b) => a.compareF(b));
+
+  public get length() {
+    return this.queue.length;
+  }
+
+  public push(node: Node): OpenSet<Node> {
+    this.queue.push(node);
+    this.ids.add(node.id());
+    return this;
+  }
+
+  public pop(): Node | undefined {
+    const node = this.queue.pop();
+    assertsIsDefined(node);
+    this.ids.delete(node.id());
+    return node;
+  }
+}
+
 export class AStar<Data extends IData, Node extends INode<Data>> implements IAStar<Data> {
   constructor(
     public successors: ISuccessors<Node>,
   ) { }
 
   public search(root: Node, goal: IGoal): Data[] | null {
-    // The open list is a priority queue of nodes to be evaluated
-    const open = new TinyQueue<Node>([root], (a, b) => a.compareF(b));
-    // The open set is a set of serialized values of nodes that have been queued
-    // We maintain this for quick duplicate checking during successor generation
-    const openSet = new Set<string>([root.id()]);
-    // The closed list is a set of nodes that have already been evaluated
-    const closed = new Set<string>();
+    const open = new OpenSet<Node>().push(root);
+    const closed = new NodeSet<Node>();
 
     while (open.length > 0) {
       const node = open.pop();
-      assertsIsDefined(node);
-      // Remove the node from the open set; it will be removed from the open queue shortly
-      openSet.delete(node.id());
       // If the node has already been evaluated, skip it
-      if (closed.has(node.id())) {
+      if (closed.has(node)) {
         continue;
       }
-      // If the node satisfies the goal, yield the path
+      // If the node satisfies the goal, return the whole path
       if (goal.satisfiedBy(node)) {
         return node.reconstruct();
       }
       // Add unsatisfying node to the closed list
-      closed.add(node.id());
+      closed.push(node);
       for (const successor of this.successors(node)) {
         // If the successor is already queued, skip it
-        if (openSet.has(successor.id())) {
+        if (open.has(successor)) {
           continue;
         }
         open.push(successor);
-        openSet.add(successor.id());
       }
     }
 
